4a898125f9c024be9b660c8b445651838779dcb1
mmedinasv05 <chavezofficial2015@gmail.com>
2022-10-26 21:04:45 -0600

    optimizing images and adding new styles

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,22 +1,22 @@
-//import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "../Components/Header";
-//import Loader from "../Components/Loader";
+import Loader from "../Components/Loader";
 import Welcome from "../Components/Welcome";
 import Footer from "../Components/Footer";
 
 export default function Home() {
-  /*const [loader, setLoader] = useState(true);
+  const [loader, setLoader] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
       setLoader(false)
     }, 1000)
-  }, [loader])*/
+  }, [loader])
 
   return (
     <>
       <Header />
-      <Welcome />
+      {(loader) ? <Loader /> : <Welcome />}
       <Footer />
     </>
   )
