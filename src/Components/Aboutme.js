c9d2a6c0bddb3332d7e0cf30ab29ad8becbadf67
Manuel Chavez <chavezofficial2015@gmil.com>
2023-10-04 17:39:35 -0600

    adding the resume option

--- src/Components/Aboutme.js
+++ src/Components/Aboutme.js
@@ -1,34 +1,51 @@
-import ListSkills from './ListSkills';
-import Manu from './Image/home-image-min.webp'
-import '../Components/CSS/Aboutme.css';
-import '../Components/CSS/CustomeProperties.css';
+import ListSkills from "./ListSkills";
+import Manu from "./Image/home-image-min.webp";
+import "../Components/CSS/Aboutme.css";
+import "../Components/CSS/CustomeProperties.css";
 
-const skillsData = ["HTML.", "CSS.", "Bootstrap.", "Javascript.", "React.", "Gitgub."]
+const skillsData = [
+  "HTML.",
+  "CSS.",
+  "Bootstrap.",
+  "Javascript.",
+  "React.",
+  "Git.",
+];
 
 function Aboutme() {
   return (
-    <main className='about-container'>
-      <section className='about-image animate__animated animate__fadeInDown'>
-        <img src={Manu} alt="Manuel Chavez" className='image-profile' />
+    <main className="about-container">
+      <section className="about-image animate__animated animate__fadeInDown">
+        <img src={Manu} alt="Manuel Chavez" className="image-profile" />
       </section>
-      <section className='about-description animate__animated animate__fadeInUp'>
-        <p className='paragraph'>
-          Soy Manuel Chavez pero puedes llamarme Manu, En el 2019 descubrí mi pasión por el desarrollo web al crear mi primera página web usando HTML y CSS desde ese entonces y hasta el día de hoy sigo sintiendo esa misma pasión cuando tengo la oportunidad de crear una página web ya que se puede crear grandes cosas combinando HTML, CSS y Javascript adicional me considero una persona autodidacta y de aprendizaje rápido ya que me gusta estar aprendiendo cosas nuevas día tras día y luego tratar de ponerlas en práctica.
+      <section className="about-description animate__animated animate__fadeInUp">
+        <p className="paragraph">
+          Soy Manuel Chavez pero puedes llamarme Manu, En el 2019 descubrí mi
+          pasión por el desarrollo web al crear mi primera página web usando
+          HTML y CSS desde ese entonces y hasta el día de hoy sigo sintiendo esa
+          misma pasión cuando tengo la oportunidad de crear una página web ya
+          que se puede crear grandes cosas combinando HTML, CSS y Javascript
+          adicional me considero una persona autodidacta y de aprendizaje rápido
+          ya que me gusta estar aprendiendo cosas nuevas día tras día y luego
+          tratar de ponerlas en práctica.
         </p>
         <br />
-        <p className='paragraph'>
-          Aunque carezco de experiencia profesional en el desarrollo web, he adquirido ciertas habilidades a través de retos personales y proyectos propios en los cuales he trabajado con algunas  tecnologías como las siguientes y actualmente busco una oportunidad que me permita crecer y desarrollarme profesionalmente en el campo del desarrollo web.
+        <p className="paragraph">
+          Aunque carezco de experiencia profesional en el desarrollo web, he
+          adquirido ciertas habilidades a través de retos personales y proyectos
+          propios en los cuales he trabajado con algunas tecnologías como las
+          siguientes y actualmente busco una oportunidad que me permita crecer y
+          desarrollarme profesionalmente en el campo del desarrollo web.
         </p>
         <br />
-        <aside className='about-skills'>
+        <aside className="about-skills">
           {skillsData.map((skill, index) => {
-            return <ListSkills key={index} skills={skill} />
+            return <ListSkills key={index} skills={skill} />;
           })}
         </aside>
       </section>
-
     </main>
-  )
+  );
 }
 
 export default Aboutme;
--- src/Components/Curriculum.js
+++ src/Components/Curriculum.js
@@ -1,28 +1,24 @@
-
 import "./CSS/Curriculum.css";
-import '../Components/CSS/CustomeProperties.css';
-import CV from './Assets/Curriculum.pdf'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDownload, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import "../Components/CSS/CustomeProperties.css";
+import CV from "./Assets/Curriculum.pdf";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
 
 function Curriculum() {
-  document.addEventListener("click", (e) => {
-    if (e.target.matches("#link-cv")) {
-      e.target.classList.add("download-cv")
-    }
-
-    setTimeout(() => {
-      e.target.classList.remove("download-cv")
-    }, 1000)
-  })
-
-
   return (
-    // <a href={CV} download="Curriculum Manuel Chavez" title="Curriculum Manuel Chavez" className="buttonDownload animate__animated animate__zoomIn" target="_blank" rel="noopener noreferrer">
-    //   Curriculum
-    // </a>
-    <a href={CV} id="link-cv" className="link-cv" download="Curriculum Manuel Chavez"><span className="text-cv">Curriculum <FontAwesomeIcon icon={faDownload} /> </span> <FontAwesomeIcon icon={faSpinner} className='icon-cv' /></a>
+    <a
+      href={CV}
+      id="link-cv"
+      className="link-cv"
+      download="Curriculum Manuel Chavez"
+      title="Descargar curriculum"
+    >
+      <span className="text-cv">Curriculum</span>
+      <span className="icon-cv">
+        <FontAwesomeIcon icon={faDownload} />
+      </span>
+    </a>
   );
 }
 
-export default Curriculum; 
+export default Curriculum;
